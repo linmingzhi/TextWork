f5bce14edcd4c6497a1faf7eb14737477bea9530
linmingzhi <mingzhi@lin.local>
2016-12-18 21:56:52 +0800

    text

---
+++ TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by lin on 12/18/16.
+//  Copyright © 2016 lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestGit
+//
+//  Created by lin on 12/18/16.
+//  Copyright © 2016 lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
